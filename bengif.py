bf8ea334c93c2ad95ab74f8ed097f4991ccc41d3
JesusGrandma <bvilli53@gmail.com>
2025-07-09 11:09:19 -0400

    fixed bengif, thx eddie

--- bengif.py
+++ bengif.py
@@ -1,19 +1,14 @@
 from discord.ext import commands
 import discord
-import os
 
 class GifCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="nip", help="benny nip gif")
+    @commands.command(name="nip", help="Send a benny nip gif by link")
     async def nip(self, ctx):
-        gif_path = "gifs/henigin1.gif"
-        if not os.path.isfile(gif_path):
-            await ctx.send("❌ GIF file not found.")
-            return
-        file = discord.File(gif_path)
-        await ctx.send(file=file)
+        gif_url = "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExNG10dzgzaXNhb2s5Nm5vZ3lnMHo2dnh4dG1vOHF2ZGE2dXYxc2dtaiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/YUQzKWdsTpq0DroDNr/giphy.gif"  # Replace with your desired GIF link
+        await ctx.send(gif_url)
 
 async def setup(bot):
     await bot.add_cog(GifCommands(bot))
