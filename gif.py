91438409d690386c03309173b0cb6392275347cd
JesusGrandma <bvilli53@gmail.com>
2025-07-09 11:51:14 -0400

    added throat

--- gif.py
+++ gif.py
@@ -12,7 +12,12 @@ class GifCommands(commands.Cog):
 
     @commands.command(name="dmonkey", help="monke dance")
     async def dance(self, ctx):
-        gif_url = "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExbG5sa2psaDM3MXA2cTg4aXYyNmFhZW92Ym9iYzBjM2ttNWk3cjUzOCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/M8kTFe827hE6c8GeN9/giphy.gif"  # Replace with your desired dance GIF link
+        gif_url = "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExbG5sa2psaDM3MXA2cTg4aXYyNmFhZW92Ym9iYzBjM2ttNWk3cjUzOCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/M8kTFe827hE6c8GeN9/giphy.gif"
+        await ctx.send(gif_url)
+
+    @commands.command(name="throat", help="dave")
+    async def dance(self, ctx):
+        gif_url = "https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExY295dmxuanNzaGkwZXp1cjdiMXFsMDY5emppYjJscGhwd3kzYTFleCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/ViJwRZ8bvMTT73YAMC/giphy.gif"  
         await ctx.send(gif_url)
 
 async def setup(bot):
